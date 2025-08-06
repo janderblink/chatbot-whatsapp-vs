c7042246ff99d70ff07c37052940e51f81b8b9c1
janderblink <janderblink@gmail.com>
2025-08-06 13:19:48 -0400

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
-app.py
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def webhook():
+    req = request.get_json()
+    print("Payload recebido:", req)
+
+    resposta = {
+        "fulfillment_response": {
+            "messages": [
+                {
+                    "text": {
+                        "text": ["Olá! Sou o Chatbot da Vigilância Sanitária."]
+                    }
+                }
+            ]
+        }
+    }
+
+    return jsonify(resposta)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=10000)
